d3850c3dee68364522bceb82595005d217a0cab6
c0b23049 <c0b2304920@edu.teu.ac.jp>
2024-10-29 18:23:58 +0900

    メインキャラクラスの実装

--- bomber_kokaton.py
+++ bomber_kokaton.py
@@ -10,20 +10,89 @@ WIDTH, HEIGHT = 750, 700
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
-DELTA = {pg.K_UP:(0, -5), 
-        pg.K_DOWN:(0, 5), 
-        pg.K_LEFT:(-5, 0), 
-        pg.K_RIGHT:(5, 0),
-        } # 練習問題1
+def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内or画面外を判定し，真理値タプルを返す関数
+    引数：こうかとんや爆弾，ビームなどのRect
+    戻り値：横方向，縦方向のはみ出し判定結果（画面内：True／画面外：False）
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 50 or WIDTH - 50 < obj_rct.right:
+        yoko = False
+    if obj_rct.top < 150 or HEIGHT - 50< obj_rct.bottom:
+        tate = False
+     
+
+    return yoko, tate
+
+
+class Hero:
+    """
+    ゲームキャラクター（こうかとん）に関するクラス
+    """
+    delta = {  # 押下キーと移動量の辞書
+        pg.K_UP: (0, -5),
+        pg.K_DOWN: (0, +5),
+        pg.K_LEFT: (-5, 0),
+        pg.K_RIGHT: (+5, 0),
+    }
+    img0 = pg.transform.rotozoom(pg.image.load("images/3.png"), 0, 0.9)
+    img = pg.transform.flip(img0, True, False)  # デフォルトのこうかとん（右向き）
+    imgs = {  # 0度から反時計回りに定義
+        (+5, 0): img,  # 右
+        (+5, -5): pg.transform.rotozoom(img, 45, 0.9),  # 右上
+        (0, -5): pg.transform.rotozoom(img, 90, 0.9),  # 上
+        (-5, -5): pg.transform.rotozoom(img0, -45, 0.9),  # 左上
+        (-5, 0): img0,  # 左
+        (-5, +5): pg.transform.rotozoom(img0, 45, 0.9),  # 左下
+        (0, +5): pg.transform.rotozoom(img, -90, 0.9),  # 下
+        (+5, +5): pg.transform.rotozoom(img, -45, 0.9),  # 右下
+    }
+
+    def __init__(self, xy: tuple[int, int]):
+        """
+        こうかとん画像Surfaceを生成する
+        引数 xy：こうかとん画像の初期位置座標タプル
+        """
+        self.img = __class__.imgs[(+5, 0)]
+        self.rct: pg.Rect = self.img.get_rect()
+        self.rct.center = xy
+        self.dire = (+5, 0) # 演習3
+
+    def change_img(self, num: int, screen: pg.Surface):
+        """
+        こうかとん画像を切り替え，画面に転送する
+        引数1 num：こうかとん画像ファイル名の番号
+        引数2 screen：画面Surface
+        """
+        self.img = pg.transform.rotozoom(pg.image.load(f"fig/{num}.png"), 0, 0.9)
+        screen.blit(self.img, self.rct)
+
+    def update(self, key_lst: list[bool], screen: pg.Surface):
+        """
+        押下キーに応じてこうかとんを移動させる
+        引数1 key_lst：押下キーの真理値リスト
+        引数2 screen：画面Surface
+        """
+        sum_mv = [0, 0]
+        for k, mv in __class__.delta.items():
+            if key_lst[k]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        self.rct.move_ip(sum_mv)
+        if check_bound(self.rct) != (True, True):
+            self.rct.move_ip(-sum_mv[0], -sum_mv[1])
+        if not (sum_mv[0] == 0 and sum_mv[1] == 0):
+            self.img = __class__.imgs[tuple(sum_mv)]
+            self.dire = sum_mv # 更新
+        screen.blit(self.img, self.rct)
 
 
 def main():
     pg.display.set_caption("ボンバーこうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("images/bg(test).png") # 一時的な背景(緑スペース背景)   
-    # kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
-    # kk_rct = kk_img.get_rect()
-    # kk_rct.center = 300, 200
+    hero = Hero((200, 300))
     clock = pg.time.Clock()
     tmr = 0
 
@@ -35,14 +104,7 @@ def main():
         screen.blit(bg_img, [0, 100]) 
 
         key_lst = pg.key.get_pressed()
-        sum_mv = [0, 0] # 横座標、縦座標
-
-        for key , tpl in DELTA.items(): # 練習問題1
-            if key_lst[key]:
-                sum_mv[0] += tpl[0] # 縦方向
-                sum_mv[1] += tpl[1] # 横方向
-
-        # kk_rct.move_ip(sum_mv)
+        hero.update(key_lst, screen)
 
         pg.display.update()
         tmr += 1
