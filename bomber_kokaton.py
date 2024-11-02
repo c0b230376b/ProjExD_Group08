2c9641d2d3b9ffa1381d4ce0ce952c007c076b26
c0a23059fb <c0a23059fb@edu.teu.ac.jp>
2024-11-02 16:15:30 +0900

    Heloクラス(こうかとん)及び画像、check_bound関数(盤面判定)結合

--- bomber_kokaton.py
+++ bomber_kokaton.py
@@ -8,41 +8,120 @@ import pygame as pg
 
 WIDTH, HEIGHT = 750, 700
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-
-DELTA = {pg.K_UP:(0, -5), 
-        pg.K_DOWN:(0, 5), 
-        pg.K_LEFT:(-5, 0), 
+DELTA = {pg.K_UP:(0, -5),
+        pg.K_DOWN:(0, 5),
+        pg.K_LEFT:(-5, 0),
         pg.K_RIGHT:(5, 0),
         } # 練習問題1
 
 
+def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内or画面外を判定し，真理値タプルを返す関数
+    引数：こうかとんや爆弾，ビームなどのRect
+    戻り値：横方向，縦方向のはみ出し判定結果（画面内：True／画面外：False）
+    """
+    yoko, tate = True, True
+
+    if obj_rct.left < 50 or WIDTH - 50 < obj_rct.right: # ブロックにぶつかったら止まるように
+        yoko = False
+    if obj_rct.top < 100 or HEIGHT - 50< obj_rct.bottom:
+        tate = False
+
+    for i in range(6):
+        num = 100*i
+        if (100 + num) < obj_rct.left < (150 + num) or (100 + num) < obj_rct.right < (150 + num):
+            for j in range(5):
+                num = 100 * j
+                if 150 + num < obj_rct.top < 200 + num or 150 + num < obj_rct.bottom < 200 + num:
+                    yoko = False
+                    tate = False
+
+    return yoko, tate
+
+
+class Hero:
+    """
+    ゲームキャラクター（こうかとん）に関するクラス
+    """
+    delta = {  # 押下キーと移動量の辞書
+        pg.K_UP: (0, -5),
+        pg.K_DOWN: (0, +5),
+        pg.K_LEFT: (-5, 0),
+        pg.K_RIGHT: (+5, 0),
+    }
+    img0 = pg.transform.rotozoom(pg.image.load("images/Kokaton/3.png"), 0, 0.9)
+    img = pg.transform.flip(img0, True, False)  # デフォルトのこうかとん（右向き）
+    imgs = {  # 0度から反時計回りに定義
+        (+5, 0): img,  # 右
+        (+5, -5): pg.transform.rotozoom(img, 45, 0.9),  # 右上
+        (0, -5): pg.transform.rotozoom(img, 90, 0.9),  # 上
+        (-5, -5): pg.transform.rotozoom(img0, -45, 0.9),  # 左上
+        (-5, 0): img0,  # 左
+        (-5, +5): pg.transform.rotozoom(img0, 45, 0.9),  # 左下
+        (0, +5): pg.transform.rotozoom(img, -90, 0.9),  # 下
+        (+5, +5): pg.transform.rotozoom(img, -45, 0.9),  # 右下
+    }
+
+    def __init__(self, xy: tuple[int, int]):
+        """
+        こうかとん画像Surfaceを生成する
+        引数 xy：こうかとん画像の初期位置座標タプル
+        """
+        self.img = __class__.imgs[(+5, 0)]
+        self.rct: pg.Rect = self.img.get_rect()
+        self.rct.center = xy
+        self.dire = (+5, 0) # 演習3
+
+    def change_img(self, num: int, screen: pg.Surface):
+        """
+        こうかとん画像を切り替え，画面に転送する
+        引数1 num：こうかとん画像ファイル名の番号
+        引数2 screen：画面Surface
+        """
+        self.img = pg.transform.rotozoom(pg.image.load(f"fig/{num}.png"), 0, 0.9)
+        screen.blit(self.img, self.rct)
+
+    def update(self, key_lst: list[bool], screen: pg.Surface):
+        """
+        押下キーに応じてこうかとんを移動させる
+        引数1 key_lst：押下キーの真理値リスト
+        引数2 screen：画面Surface
+        """
+        sum_mv = [0, 0]
+        for k, mv in __class__.delta.items():
+            if key_lst[k]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        self.rct.move_ip(sum_mv)
+        if check_bound(self.rct) != (True, True):
+            self.rct.move_ip(-sum_mv[0], -sum_mv[1])
+        if not (sum_mv[0] == 0 and sum_mv[1] == 0):
+            self.img = __class__.imgs[tuple(sum_mv)]
+            self.dire = sum_mv # 更新
+        screen.blit(self.img, self.rct)
+
+
 def main():
+    """
+    ゲームのメイン処理を行う関数
+    """
     pg.display.set_caption("ボンバーこうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("images/bg_ver.1.0.png")
-    # kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
-    # kk_rct = kk_img.get_rect()
-    # kk_rct.center = 300, 200
+    bg_img = pg.image.load("images/bg_ver.1.0.png") # 背景(完成版)
+    hero = Hero((75, 125))
     clock = pg.time.Clock()
     tmr = 0
 
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: 
+            if event.type == pg.QUIT:
                 return
-            
-        screen.blit(bg_img, [0, 50]) 
+
+        screen.blit(bg_img, [0, 50])
 
         key_lst = pg.key.get_pressed()
-        sum_mv = [0, 0] # 横座標、縦座標
-
-        for key , tpl in DELTA.items(): # 練習問題1
-            if key_lst[key]:
-                sum_mv[0] += tpl[0] # 縦方向
-                sum_mv[1] += tpl[1] # 横方向
-
-        # kk_rct.move_ip(sum_mv)
+        hero.update(key_lst, screen)
 
         pg.display.update()
         tmr += 1
